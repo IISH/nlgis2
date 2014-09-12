bcc95682034d8a82baf8a75a5ce9f3d89265f4cb
root <4tikhonov@gmail.com>
2014-09-12 17:07:03 +0200

    Extended with topojson convertor support

--- maps/bin/batch_loader.py
+++ maps/bin/batch_loader.py
@@ -4,18 +4,27 @@ from os import listdir
 from os.path import isfile, join
 import sys
 import re
-from subprocess import call
-call(["ls", "-l"])
+import os
+import subprocess
 
 mypath = sys.argv[1]
+topodir = '../topojson'
+
+if not os.path.exists(topodir):
+    os.makedirs(topodir)
 
 files = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 for file in files:
 	filepath = mypath + '/' + file
 	m = re.search('(\d+)', file)
 	year = m.group(0)
+	topopath = topodir + '/' + year + '.json'
 
         upload = 'json2mongodb.py ' + filepath + ' ' + year
-        call(["./json2mongodb.py", filepath, year])
+	toporun = '/usr/bin/topojson -o ' + topopath + ' ' + filepath
+	print toporun
+	subprocess.call(["/usr/bin/topojson", "-o", topopath, filepath])
+	if os.path.isfile(topopath) and os.access(topopath, os.R_OK):
+        	subprocess.call(["./json2mongodb.py", topopath, year])
 	print upload
 
