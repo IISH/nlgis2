886851f98f285121b4ebdff7cc5a4576d7167359
Vyacheslav Tykhonov <root@node-128.dev.socialhistoryservices.org>
2014-09-19 17:13:23 +0200

    API service for maps and datasets

---
+++ web/api/api.py
@@ -0,0 +1,209 @@
+from flask import Flask, Response, request
+from twisted.web import http
+import json
+import urllib2
+import glob
+import csv
+import xlwt
+import os
+import sys
+import psycopg2
+import psycopg2.extras
+import pprint
+import collections
+import getopt
+import ConfigParser
+from subprocess import Popen, PIPE, STDOUT
+
+def connect():
+        cparser = ConfigParser.RawConfigParser()
+        cpath = "/etc/apache2/nlgiss2.config"
+        cparser.read(cpath)
+
+	conn_string = "host='%s' dbname='%s' user='%s' password='%s'" % (cparser.get('config', 'dbhost'), cparser.get('config', 'dbname'), cparser.get('config', 'dblogin'), cparser.get('config', 'dbpassword'))
+
+    	# get a connection, if a connect cannot be made an exception will be raised here
+    	conn = psycopg2.connect(conn_string)
+
+    	# conn.cursor will return a cursor object, you can use this cursor to perform queries
+    	cursor = conn.cursor()
+
+    	#(row_count, dataset) = load_regions(cursor, year, datatype, region, debug)
+	return cursor
+
+def json_generator(c, jsondataname, data):
+	sqlnames = [desc[0] for desc in c.description]
+        jsonlist = []
+        jsonhash = {}
+        
+        for valuestr in data:    
+            datakeys = {}
+            for i in range(len(valuestr)):
+               name = sqlnames[i]
+               value = valuestr[i]
+               datakeys[name] = value
+               #print "%s %s", (name, value)
+            jsonlist.append(datakeys)
+        
+        jsonhash[jsondataname] = jsonlist;
+        json_string = json.dumps(jsonhash, encoding="utf-8", sort_keys=True, indent=4)
+
+        return json_string
+
+def load_years(cursor):
+        data = {}
+        sql = "select * from datasets.years where 1=1";
+        # execute
+        cursor.execute(sql)
+
+        # retrieve the records from the database
+        data = cursor.fetchall()
+        json_string = json.dumps(data, encoding="utf-8")
+
+        return json_string
+
+def sqlfilter(sql):
+        items = ''
+        sqlparams = ''
+
+	for key, value in request.args.items():
+            items = request.args.get(key, '')
+            itemlist = items.split(",")
+            for item in itemlist:
+                sqlparams = "\'%s\',%s" % (item, sqlparams)
+            sqlparams = sqlparams[:-1]
+            sql += " AND %s in (%s)" % (key, sqlparams)
+	return sql
+
+def load_topics(cursor):
+        data = {}
+        sql = "select * from datasets.topics where 1=1";
+	sql = sqlfilter(sql) 
+
+        # execute
+        cursor.execute(sql)
+
+        # retrieve the records from the database
+        data = cursor.fetchall()
+        jsondata = json_generator(cursor, 'data', data)
+        
+        return jsondata
+
+def load_classes(cursor):
+        data = {}
+        sql = "select * from datasets.histclasses where 1=1";
+        sql = sqlfilter(sql)
+
+        # execute
+        cursor.execute(sql)
+
+        # retrieve the records from the database
+        data = cursor.fetchall()
+        jsondata = json_generator(cursor, 'data', data)
+
+        return jsondata
+
+def load_regions(cursor):
+        data = {}
+        sql = "select * from datasets.regions where 1=1";
+	sql = sqlfilter(sql)
+	sql = sql + ';'
+        # execute
+        cursor.execute(sql)
+
+        # retrieve the records from the database
+        data = cursor.fetchall()
+	jsondata = json_generator(cursor, 'regions', data)
+
+        return jsondata
+
+def load_data(cursor, year, datatype, region, debug):
+        data = {}
+
+        # execute our Query
+	#    for key, value in request.args.iteritems():
+	#        extra = "%s<br>%s=%s<br>" % (extra, key, value)
+
+        query = "select * from data WHERE 1 = 1 ";
+	query = sqlfilter(query)
+        if debug:
+            print "DEBUG " + query + " <br>\n"
+        query += ' order by territory asc'
+
+        # execute
+        cursor.execute(query)
+
+        # retrieve the records from the database
+        records = cursor.fetchall()
+
+        row_count = 0
+        i = 0
+        for row in records:
+                i = i + 1
+                data[i] = row
+#               print row[0]
+	jsondata = json_generator(cursor, 'data', records)
+
+        return jsondata;
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    description = 'nlgis2 API Service v.0.1<br>/api/maps (map polygons)<br>/api/data (data services)<br>'
+    return description
+
+@app.route('/topics')
+def topics():
+    cursor = connect()
+    data = load_topics(cursor)
+    return Response(data,  mimetype='application/json')
+
+@app.route('/histclasses')
+def classes():
+    cursor = connect()
+    data = load_classes(cursor)
+    return Response(data,  mimetype='application/json')
+
+@app.route('/years')
+def years():
+    cursor = connect()
+    data = load_years(cursor)
+    return Response(data,  mimetype='application/json')
+
+@app.route('/regions')
+def regions():
+    cursor = connect()
+    data = load_regions(cursor)
+    return Response(data,  mimetype='application/json')
+
+@app.route('/data')
+def data():
+    cursor = connect()
+    year = 0
+    datatype = '1.01'
+    region = 0
+    debug = 0
+    data = load_data(cursor, year, datatype, region, debug)
+    return Response(data,  mimetype='application/json')
+
+@app.route('/maps')
+def maps():
+
+    # Default year is 1997
+    year = 1997
+    paramyear = request.args.get('year');
+    if paramyear:
+	year = paramyear
+
+    cmd = "/home/slava/nlgis2/maps/bin/topojson.py " + str(year)
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    response = json.dumps(p.stdout.read())
+    #"objects":{"1812
+    #new_string = re.sub(r'"{\"1812"', r'{\"NLD', response)
+    json_response = json.loads(response)
+
+    return Response(json_response,  mimetype='application/json;charset=utf-8')
+
+if __name__ == '__main__':
+    app.run()
