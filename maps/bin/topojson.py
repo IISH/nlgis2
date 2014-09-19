ae5656bacf2ff6b1e2162877cd6991166e704caf
Vyacheslav Tykhonov <root@node-128.dev.socialhistoryservices.org>
2014-09-19 17:10:12 +0200

    Updated with improved support of Netherlands map

--- maps/bin/topojson.py
+++ maps/bin/topojson.py
@@ -2,6 +2,9 @@
 
 from pymongo import MongoClient
 import sys
+import json
+import ast
+import re
 
 year = sys.argv[1]
 country = 'NLD'
@@ -11,5 +14,13 @@ db = client.histboundaries  # use a database called boundaries to store json
 collection = db.json   # and inside that DB, a collection called "files"
 
 polygons = collection.find({ 'year': year }) 
-for json in polygons:
-	print json
+for jsonstr in polygons:
+	newjson = dict(jsonstr.items())
+	for key in newjson:
+	    if key == 'json':
+		response = newjson[key] 
+		#new_string = re.sub(r'cts\"\:\{\"1812', r'cts":{"NLD', response)	
+		replace = 'cts\"\:\{\"'+str(year)
+		new_string = re.sub(replace, r'cts":{"nld', response)
+	        print new_string
+
