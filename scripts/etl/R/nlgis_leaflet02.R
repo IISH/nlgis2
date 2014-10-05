600e7a74aa668aa90986a9b02fd07be77a8018b7
rlzijdeman <richard.zijdeman@gmail.com>
2014-10-05 22:07:23 +0200

    mapping geojson in R working examples
    - downloading map & data from API
    - merging data & API
    - plotting with spplot & leafletR (with popup)
    NB: muni’s are wrongly placed!!!!

--- scripts/etl/R/nlgis_leaflet02.R
+++ scripts/etl/R/nlgis_leaflet02.R
@@ -17,7 +17,7 @@ view.nlg <- function(year, name){
 }
 # to run script use something like:
 # view.nlg(year = 1842, name = "its_working")
-view.nlg(1975, "testmap")
+view.nlg(1812, "testmap")
 
 ## installing new dev version of leafletR
 if(!require(devtools)) install.packages("devtools")
@@ -30,8 +30,103 @@ view.nlt <- function(year, name){
                        destfile = paste0(name, ".json"))
   topomap <- leaflet(paste0("./", paste0(name, ".json")),
                     title = paste0("Netherlands - ", year))
-  browseURL(topomap)      
+  #browseURL(topomap)      
 }
-view.nlt(1812, "nld_topo_1812")
+testmapview.nlt(1812, "nld_topo_1812")
 
+library(jsonlite)
+testdata <- as.data.frame(
+  fromJSON("http://node-128.dev.socialhistoryservices.org/api/data?year=1977"))
+
+test.dat <- toGeoJSON(data=testdata[1:100,], dest=tempdir(), name="test_data")
+
+
+
+
+testmap <- view.nlt(1812, "nld_topo_1812")
+
+
+#
+library(leafletR)
+library(jsonlite)
+download.file("http://node-128.dev.socialhistoryservices.org/api/maps?year=1937",
+                     destfile = paste0("testmap", ".json"))
+map <- leaflet("./testmap.json")
+map # displays map in browser
+
+
+test <- readOGR(dsn = "./testmap.json", driver="GeoJSON")
+
+testdata <- 
+  fromJSON("http://node-128.dev.socialhistoryservices.org/api/data?code=TXCU")
+
+table(testdata$data$year)
+
+map.shape <- spTransform(map)
+
+#####
+require(rgdal) 
+any(grepl("GeoJSON", ogrDrivers()$name)) ### TRUE 
+url <- " 
+https://raw.github.com/johan/world.geo.json/master/countries.geo.json" 
+world <- readOGR(dsn = url, layer = "OGRGeoJSON") 
+plot(world) 
+
+### attempt to use data and geojson api to plot results
+library(leafletR)
+library(jsonlite)
+library(rgdal)
+library(sp)
+
+
+# get data
+testdata <- 
+  fromJSON("http://node-128.dev.socialhistoryservices.org/api/data?year=1977")
+
+# get map (and showmap using leaflet)
+download.file(
+  "http://node-128.dev.socialhistoryservices.org/api/maps?year=1977&format=geojson",
+              destfile = paste0("testmap2", ".geojson"))
+
+# mapdata <- fromJSON(
+#   "http://node-128.dev.socialhistoryservices.org/api/maps?year=1977&format=geojson"
+#   )
+
+map = readOGR("testmap2.geojson", "OGRGeoJSON")
+plot(map)
+
+#subsetting testdata (due to different 'codes'
+# unsure of meaning, just selecting a single one)
+testdata.sub <- subset(testdata$data, code == "TEPV", 
+                       select = c("amsterdam_code",
+                                  "value"))
+table(duplicated(testdata.sub$amsterdam_code))
+testdata.sub1 <- subset(testdata.sub, !duplicated(amsterdam_code))
+                        
+map@data <- merge(map@data, testdata.sub1,
+                  by.x = "amsterdamcode",
+                  by.y = "amsterdam_code",
+                  all.x = TRUE)
+
+at = c(7,500,1000,3194, 2488, 10000, 10000)
+col = rev(heat.colors(length(at)))
+
+
+mapvalues <- spplot(map,"value", at = at, col.regions = col, 
+                        cuts = length(at),
+                        main=list(label="value of TK", cex=2))
+mapvalues # works!
+
+
+writeOGR(map, "./mapTK2.geojson", layer = "value", driver='GeoJSON') 
+writeOGR(map, "./mapTK3.geojson", layer = c("value", "naam"), driver='GeoJSON') 
+# NB error upon overwrite
+
+cuts = c(7,500,1000,3194, 2488, 10000, 10000)
+sty<-styleGrad(prop="value", breaks=cuts, right=FALSE, style.par="col",
+               style.val=rev(heat.colors(6)), leg="value TK 1977", lwd=1)
+popup<-c("name", "value")
+lfmap <- leaflet("./mapTK.geojson", incl.data = TRUE, popup=popup, style = sty)
+
+lfmap # displays map in browser
 
