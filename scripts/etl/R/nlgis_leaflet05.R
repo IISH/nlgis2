bc351d60bdf9baf8e1015406442ca7a2c615ae3b
rlzijdeman <richard.zijdeman@gmail.com>
2014-10-09 21:23:04 +0200

    nlgget.R
    New function for complex queries of data API

--- scripts/etl/R/nlgis_leaflet05.R
+++ scripts/etl/R/nlgis_leaflet05.R
@@ -1,7 +1,7 @@
 # File: nlgis_leaflet05.R
 # Date: Oct 6, 2014
 # Author: richard.zijdeman@iisg.nl
-# Last change: none so far
+# Last change: starting to work on functions to access api's
 
 # clean workspace
 rm(list=ls())
@@ -13,8 +13,68 @@ library(rgdal)
 library(sp)
 
 # get data
+# somedata <- 
+#   fromJSON("http://node-128.dev.socialhistoryservices.org/api/data?year=1937")
+# 
 somedata <- 
-  fromJSON("http://node-128.dev.socialhistoryservices.org/api/data?year=1937")
+   fromJSON("http://node-128.dev.socialhistoryservices.org/api/data?year=1937&code=TXCU")
+
+
+# function to get data ####
+nlgget <- function(year, code, name) {
+  year.add <- paste0("&year=", year)
+  code.add <- paste0("&code=", toupper(code))
+  name.add <- paste0("&naam=", toupper(name))
+  url <- "http://node-128.dev.socialhistoryservices.org/api/data"
+  
+  if (is.na(year) & is.na(code) & is.na(name)){
+    data.url <- url
+  }
+   else {
+     url <-paste0(url, "?")
+     data.url <- paste0(paste0(paste0(url,year.add),code.add),name.add)
+     # fixing wrong entries:
+     data.url <- gsub("&year=NA", "", data.url)
+     data.url <- gsub("&code=NA", "", data.url)
+     data.url <- gsub("&naam=NA", "", data.url)
+     data.url <- gsub("\\?&",    "?", data.url)
+   }
+  
+  #print(data.url) #uncomment for test output of url
+  fromJSON(data.url)
+}
+# nlgget(year=1937, code = "TXCU", name = "BAFLO")
+
+
+
+###############
+#x
+t2 <- nlgget(year=1937, code = "TxCU", name = "BaFLO")
+
+
+nlgget(year=1978)
+t.1978.adam      <- nlgget(year=1978, name = "AMSTERDAM")
+t.1978.adam.txcu <- nlgget(year=1978, name = "Amsterdam", code = "TXCU")
+
+
+nlgplot()
+
+
+
+
+
+nlgget(12)
+
+
+test.data <- nlgget()
+
+
+temp <- nlgget()
+nlgget("1937", "")
+nlgget(12,"weirdass")
+
+
+
 
 # select relevant subset
 somedata.1 <- subset(somedata$data, code == "TXRK", 
---
+++ scripts/etl/R/package/nlgget.R
@@ -0,0 +1,25 @@
+library(jsonlite)
+
+nlgget <- function(year, code, name) {
+  year.add <- paste0("&year=", year)
+  code.add <- paste0("&code=", toupper(code))
+  name.add <- paste0("&naam=", toupper(name))
+  url <- "http://node-128.dev.socialhistoryservices.org/api/data"
+  
+  if (is.na(year) & is.na(code) & is.na(name)){
+    data.url <- url
+  }
+  else {
+    url <-paste0(url, "?")
+    data.url <- paste0(paste0(paste0(url,year.add),code.add),name.add)
+    # fixing wrong entries:
+    data.url <- gsub("&year=NA",  "", data.url)
+    data.url <- gsub("&code=NA",  "", data.url)
+    data.url <- gsub("&naam=NA",  "", data.url)
+    data.url <- gsub(    "\\?&", "?", data.url)
+  }
+  
+  #print(data.url) #uncomment for test output of url
+  fromJSON(data.url)
+}
+
