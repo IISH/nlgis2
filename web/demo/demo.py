7ffe110e4d70bce029ff388d61228d62c3b51cae
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2014-11-04 17:44:57 +0100

    Authorship added

--- web/demo/demo.py
+++ web/demo/demo.py
@@ -1,3 +1,30 @@
+# Copyright (C) 2014 International Institute of Social History.
+# @author Vyacheslav Tykhonov <vty@iisg.nl>
+#
+# This program is free software: you can redistribute it and/or  modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# As a special exception, the copyright holders give permission to link the
+# code of portions of this program with the OpenSSL library under certain
+# conditions as described in each individual source file and distribute
+# linked combinations including the program with the OpenSSL library. You
+# must comply with the GNU Affero General Public License in all respects
+# for all of the code used other than as permitted herein. If you modify
+# file(s) with this exception, you may extend this exception to your
+# version of the file(s), but you are not obligated to do so. If you do not
+# wish to do so, delete this exception statement from your version. If you
+# delete this exception statement from all source files in the program,
+# then also delete it in the license file.
+
 from flask import Flask, render_template
 from flask import g
 from flask import Response, make_response, request
@@ -169,6 +196,10 @@ def slider():
     #return 'slider'
     return render_template('slider.html')
 
+@app.route('/members')
+def slider():
+    return render_template('members.html')
+
 @app.route('/d3map')
 def d3map(settings=''):
     (year, code, website, server, imagepathloc, imagepathweb, viewerpath, path, geojson, datarange, custom) = readglobalvars()
@@ -235,7 +266,9 @@ def d3site(settings=''):
     template = 'site_tabs.html'
     if custom:
         template = 'site_tabs_custom.html'
-    resp = make_response(render_template(template, topojsonurl=apiurl, datajsonurl=dataapiurl, datayear=year, codes=codes, indicators=indicators, datarange=datarange, selectedcode=selectedcode, thiscode=code, showlegend=showlegend, allyears=years, custom=custom, custom_indicators=custom_indicators, custom_allyears=custom_years))
+
+    legendscales = ["100-200","50-99", "10-49", "1-9", "0-1"]
+    resp = make_response(render_template(template, topojsonurl=apiurl, datajsonurl=dataapiurl, datayear=year, codes=codes, indicators=indicators, datarange=datarange, selectedcode=selectedcode, thiscode=code, showlegend=showlegend, allyears=years, custom=custom, custom_indicators=custom_indicators, custom_allyears=custom_years, legendscales=legendscales))
     return resp
 
 @app.route('/download')
