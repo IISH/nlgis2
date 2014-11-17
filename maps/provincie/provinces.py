f7390086c7daa1c1aaa9fa29fc1a72b78c3373e5
Tikhonov Vyacheslav <4tikhonov@gmail.com>
2014-11-17 14:12:32 +0100

    Added conversion to topojson

--- maps/provincie/provinces.py
+++ maps/provincie/provinces.py
@@ -1,25 +1,29 @@
-#!/usr/bin/python
-# Use case for NLGIS2 geo api service
+
+# coding: utf-8
+
+# In[ ]:
+
+# NLGIS2 municipalities loader
 # (C) Vyacheslav Tykhonov vty@iisg.nl
 # International Institute of Social History 
 # http://socialhistory.org
 
-#%matplotlib inline
+get_ipython().magic(u'matplotlib inline')
 import urllib2
 import simplejson
 import json
 import sys
 import pandas as pd
 import random
-#import vincent
+import vincent
 import codecs
-#from vincent import Axis, AxisProperties, PropertySet, ValueRef
+from vincent import Axis, AxisProperties, PropertySet, ValueRef
 from subprocess import Popen, PIPE, STDOUT
 
 # Global settings
 provinceurl = "http://www.gemeentegeschiedenis.nl/provincie/json/"
 geoapiurl = 'http://node-128.dev.socialhistoryservices.org/api/maps?format=geojson&year='
-thisprovince = 'Noord-Holland'
+#thisprovince = 'Noord-Holland'
 #thisprovince = 'Flevoland'
 
 def load_remote_data(apiurl, param):
@@ -34,17 +38,19 @@ def load_remote_data(apiurl, param):
 provlist = ["Groningen", "Friesland", "Drenthe", "Overijssel", "Flevoland", "Gelderland", "Utrecht", "Noord-Holland", "Zuid-Holland", "Zeeland", "Noord-Brabant", "Limburg"]
 provincies = {}
 amscodes = {}
-if thisprovince:
-    provlist = []
-    provlist.append(thisprovince)
+#if thisprovince:
+    #provlist = []
+    #provlist.append(thisprovince)
     
 for province in provlist:
+    print province
     data = load_remote_data(provinceurl, province)
-    #data
     provincelist = []
+    loclist = ''
     for item in data:
         locations = {}
         #print item['amco'] + ' ' + item['provincie'] + ' ' + item['startjaar'] + ' ' + item['eindjaar'] + ' ' + item['naam']
+        loclist = loclist + str(item['amco']) + ', '
         locations['amsterdamcode'] = item['amco']
         locations['name'] = item['naam']
         locations['start'] = item['startjaar']
@@ -52,22 +58,39 @@ for province in provlist:
         amscodes[locations['amsterdamcode']] = item['startjaar']
         locations['cbscode'] = item['cbscode']
         provincelist.append(locations)
-    provincies[province] = provincelist
+    loclist = loclist.replace(' ', '')[:-1].upper()
+    provincies[province] = loclist
     
-years = [1997]
+#years = [1980, 1997]
+for i in sorted(range(1812, 1997)):
+    years.append(i)
 mapjson = {}
-dir = "/home/slava/nlgis2/maps/provincie/json"
+dir = "/home/tikhonov/dev/maps/json"
 outdir = dir + "/../provinces"
 country = 'nld'
-for year in years:
-    data = load_remote_data(geoapiurl, str(year))
-    filename = dir + '/' + str(year) + '.json'
-    outfile = outdir + '/out' + str(year) + '_' + thisprovince + '.json'
-    file = codecs.open(filename, "w", "utf-8")
-    file.write(json.dumps(data, encoding="utf-8", sort_keys=True, indent=4))
-    file.close()
-    cmd = "ogr2ogr   -f GeoJSON   -where \"amsterdamcode in (11150,10822) \"  " + outfile + " " + filename
-    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    response = p.stdout.read()    
-    print response
+for province in provlist:
+    loclist = provincies[province]
+    for year in years:
+        try:
+            # Get geojson from GEO API
+            data = load_remote_data(geoapiurl, str(year))
+            filename = dir + '/' + str(year) + '.json'
+            outfile = outdir + '/geojson_' + str(year) + '_' + province + '.json'
+            topofile = outdir + '/topojson_' + str(year) + '_' + province + '.json'
+            file = codecs.open(filename, "w", "utf-8")
+            file.write(json.dumps(data, encoding="utf-8", sort_keys=True, indent=4))
+            file.close()
+            # Extract polygons for specific province
+            cmd = "/usr/bin/ogr2ogr   -f GeoJSON   -where \"amsterdamcode in (" + loclist + ") \"  " + outfile + " " + filename
+            p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+            response = p.stdout.read()    
+            # Convert to topojson
+            toporun = '/usr/bin/topojson -o ' + topofile + ' --id-property=+amsterdamcode -p -- ' + filename
+            p = Popen(toporun, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+            response = p.stdout.read()
+            print response
+        except:
+            print "Warning: can't read geojson for " + str(year)
+    
 mapjson
+
